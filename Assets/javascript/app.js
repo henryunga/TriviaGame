5a5d7fe477a1bd589e65fe508ba0457b8d47cf6d
Henry Unga <HenryUnga@calvisitor-10-105-168-105.calvisitor.1918.berkeley.edu>
2017-06-06 17:10:08 -0700

    Fixed Game

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+//note
 function initialScreen() {
 	startScreen = "<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Start Quiz</a></p>";
 	$(".mainArea").html(startScreen);
